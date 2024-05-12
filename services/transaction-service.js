33ad2906302ba60b94c0bf47e749ca85828e8d37
phandung2101 <dungpv.dev@gmail.com>
2024-05-13 06:44:01 +0700

    [ADD] service test

---
+++ services/transaction-service.js
@@ -0,0 +1,13 @@
+const HTTP = require('./base-api-service');
+
+module.exports = {
+    add: (transaction, token) => {
+        return HTTP.post('/transactions', transaction, config(token));
+    },
+
+    config: token => ({
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+}
